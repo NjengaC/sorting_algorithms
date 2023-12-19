b2620ca8fc294a40e065944f28eba69bb4209d46
NjengaC <victorcyrus01@gmail.com>
2023-12-20 00:32:58 +0300

    sport

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -3,28 +3,35 @@
 /**
  * heap_tree - performs a sift-down operation to maintain the heap property
  * @array: array
- * @size: size of the array
- * @base: size of the heap
- * @root: index to start form
+ * @n: size of the array
+ * @size: size of the heap
+ * @index: index to start form
  */
-void heap_tree(int *array, size_t size, size_t base, size_t root)
+void heap_tree(int *array, size_t n, size_t size, size_t index)
 {
-	size_t left, right, large;
+	size_t s1, s2;
+	int val1, val2, tmp;
 
-	left = 2 * root + 1;
-	right = 2 * root + 2;
-	large = root;
+	s1 = 2 * index + 1;
+	s2 = 2 * index + 2;
+	val1 = (s1 < size) ? array[s1] : INT_MIN;
+	val2 = (s2 < size) ? array[s2] : INT_MIN;
 
-	if (left < base && array[left] > array[large])
-		large = left;
-	if (right < base && array[right] > array[large])
-		large = right;
-
-	if (large != root)
+	if (val1 > val2 && val1 > array[index])
 	{
-		Swap(array + root, array + large);
-		print_array(array, size);
-		heap_tree(array, size, base, large);
+		tmp = array[index];
+		array[index] = array[s1];
+		array[s1] = tmp;
+		print_array(array, n);
+		heap_tree(array, n, size, s1);
+	}
+	else if (val2 > val1 && val2 > array[index])
+	{
+		tmp = array[index];
+		array[index] = array[s2];
+		array[s2] = tmp;
+		print_array(array, n);
+		heap_tree(array, n, size, s2);
 	}
 }
 
