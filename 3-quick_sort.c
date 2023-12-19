2c589384a998c4c8f22fed37d90d7368e45b3aba
NjengaC <victorcyrus01@gmail.com>
2023-12-19 23:35:24 +0300

    lomuto

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,43 +1,44 @@
 #include "sort.h"
 
 /**
- * partition - Partitions an array using Hoare's scheme.
- * @array: The array to partition.
- * @low: The low index of the partition.
- * @high: The high index of the partition.
- * @size: The size of the array.
- * Return: The pivot index.
+ * partition - partitions an array of integers using Lomuto partiton scheme
+ * @array: The array
+ * @low: lower index of the partition
+ * @high: higher end index
+ * @size: size of array
+ * Return: pivot index.
  */
 int partition(int *array, int low, int high, size_t size)
 {
-	int pivot = array[low];
+
+	int pivot = array[high];
 	int i = low - 1;
-	int j = high + 1;
+	int j;
 
-
-	while (1)
+	for (j = low; j <= high - 1; j++)
 	{
-		do {
+		if (array[j] <= pivot)
+		{
 			i++;
-		} while (array[i] < pivot);
-		do {
-			j--;
-		} while (array[j] > pivot);
-
-		if (i >= j)
-			return (j);
-
-		Swap(&array[i], &array[j]);
-		print_array(array, size);
+			Swap(&array[i], &array[j]);
+			print_array(array, size);
+		}
 	}
+
+	Swap(&array[i + 1], &array[high]);
+	print_array(array, size);
+
+	return i + 1;
 }
 
+
 /**
- * quicksort - Sorts an array using the quicksort algorithm.
- * @array: The array to sort.
- * @low: The low index of the array.
- * @high: The high index of the array.
- * @size: The size of the array.
+ * quicksort - sorts an array using the quicksort algorithm.
+ * @array: array
+ * @low: low index
+ * @high: high index
+ * @size: size of the array
+ * Return: nothing
  */
 void quicksort(int *array, int low, int high, size_t size)
 {
@@ -45,15 +46,15 @@ void quicksort(int *array, int low, int high, size_t size)
 	{
 		int pivot = partition(array, low, high, size);
 
-		quicksort(array, low, pivot, size);
+		quicksort(array, low, pivot - 1, size);
 		quicksort(array, pivot + 1, high, size);
 	}
 }
 
 /**
- * quick_sort - Initiates the quicksort algorithm.
- * @array: The array to sort.
- * @size: The size of the array.
+ * quick_sort - main function calling
+ * @array: the array
+ * @size: size of array
  */
 void quick_sort(int *array, size_t size)
 {
